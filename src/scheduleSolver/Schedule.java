4ee8a8dd2fb6d179bd65b9944fcda2c3cebcb023
Vinh Dang <dtt.vinh@gmail.com>
2015-01-22 15:42:13 -0700

    Add Guava lib for primitive types handling. Finish events_spaces constraint method in Schedule class.

--- src/scheduleSolver/Schedule.java
+++ src/scheduleSolver/Schedule.java
@@ -2,9 +2,13 @@ package scheduleSolver;
 
 import java.util.ArrayList;
 
+import com.google.common.primitives.Ints;
+
 import solver.Solver;
 import solver.constraints.Constraint;
 import solver.constraints.IntConstraintFactory;
+import solver.variables.IntVar;
+import solver.variables.VariableFactory;
 
 public class Schedule {
 	public final String name;
@@ -21,14 +25,48 @@ public class Schedule {
 		this.solver = new Solver(this.name);
 	}
 	
-//	public Constraint getConstraintsEventsSpaces() {
-//		Constraint tmp = IntConstraintFactory.TRUE(this.solver);
-//		ArrayList<Constraint> constraints = new ArrayList<Constraint>();
-//		for (Event event : this.events) {
-//			//IntVariable
-//		}
-//		return tmp;
-//	}
+	public Constraint getConstraintsEventsSpaces() {
+		ArrayList<IntVar> timeBlocks = new ArrayList<IntVar>();
+		for (Event event : this.events) {
+			// 1. Extract domain from event.possibleStartTimes
+			//		Things to consider:
+			//		- Convert from human-friendly strings to integers
+			//		- Space's capacity
+			//		- Space's availability (can just ignore for now...)
+			//    For now, we will assume that possibleStartTimes are in
+			//      correct format already (010910 instead of T:09:10 for instance)
+			// 2. time0 = IntVariable.enumerated(domain)
+			// 3. populate array of time_offset (c1_s1_t0, c1_s1_t1, c1_s1_t2...)
+			// 4. Do the same for all the events
+			// 5. create alldifferent(array) constraint
+			
+			// 1.
+			ArrayList<Integer> domain = new ArrayList<Integer>();
+			for (String t : event.possibleStartTimes) {
+				Integer tmp = Integer.parseInt(t) * 100;
+				for (Space space : this.spaces) {
+					if (space.capacity >= event.maxParticipants) {
+						domain.add(tmp + space.id);
+					}
+				}
+			}
+			
+			// 2.
+			IntVar time0 = VariableFactory.enumerated("start time",
+					Ints.toArray(domain), this.solver);
+			
+			// 3.
+			for (int i = 0; i < event.duration; i += 5) {
+				IntVar timeVar = VariableFactory.offset(time0, i);
+				timeBlocks.add(timeVar);
+			}
+			
+			// 4. -- Continue the loop
+		}
+		
+		// 5.
+		return IntConstraintFactory.alldifferent((IntVar[]) timeBlocks.toArray());
+	}
 	
 	/**
 	 * @param args
@@ -38,8 +76,8 @@ public class Schedule {
 
 	}
 	
-	public class Event {}
-	public class Space {}
-	public class Time {}
+	public class Space {
+		int id, capacity;
+	}
 
 }
