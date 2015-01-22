08fd231c97c2d7bb5df46145a81b934a945da278
Vinh Dang <dtt.vinh@gmail.com>
2015-01-22 14:52:52 -0700

    Add choco lib. Update Person and Schedule classes.

--- src/scheduleSolver/Person.java
+++ src/scheduleSolver/Person.java
@@ -1,5 +1,20 @@
 package scheduleSolver;
 
 public class Person {
+	public final String name;
+	public final String id; // or int?
+	
+	public Person(String _name, String _id) {
+		this.name = _name;
+		this.id = _id;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- src/scheduleSolver/Schedule.java
+++ src/scheduleSolver/Schedule.java
@@ -1,5 +1,45 @@
 package scheduleSolver;
 
+import java.util.ArrayList;
+
+import solver.Solver;
+import solver.constraints.Constraint;
+import solver.constraints.IntConstraintFactory;
+
 public class Schedule {
+	public final String name;
+	public final Event events[];
+	public final Space spaces[];
+	public final Person persons[];
+	private Solver solver;
+	
+	public Schedule(String name, Event events[], Space spaces[], Person persons[]) {
+		this.name = name;
+		this.events = events;
+		this.spaces = spaces;
+		this.persons = persons;
+		this.solver = new Solver(this.name);
+	}
+	
+//	public Constraint getConstraintsEventsSpaces() {
+//		Constraint tmp = IntConstraintFactory.TRUE(this.solver);
+//		ArrayList<Constraint> constraints = new ArrayList<Constraint>();
+//		for (Event event : this.events) {
+//			//IntVariable
+//		}
+//		return tmp;
+//	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public class Event {}
+	public class Space {}
+	public class Time {}
 
 }
