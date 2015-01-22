b30fb0a7074b997d8ba5d716b48f13cdebb28016
Tony <tony.tuttle@utah.edu>
2015-01-22 14:55:09 -0700

    skeletonizing Event.java

--- src/scheduleSolver/Event.java
+++ src/scheduleSolver/Event.java
@@ -1,5 +1,13 @@
 package scheduleSolver;
 
-public class Event {
+import java.util.ArrayList;
 
+public class Event {
+	String name;
+	String ID;
+	int daysCount;
+	int duration;
+	ArrayList<String> possibleStartTimes; /* for now these are Strings, maybe a pair such as <String:int> or <String:Time> */
+	char[] days;
+	
 }
