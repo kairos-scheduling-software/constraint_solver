1ab8b325882ac8f8eedc7998be42511a9ee73e52
Tony <tony.tuttle@utah.edu>
2015-01-22 17:49:44 -0700

    basic code

--- src/scheduleSolver/Time.java
+++ src/scheduleSolver/Time.java
@@ -1,5 +1,79 @@
 package scheduleSolver;
 
 public class Time {
-
+	/* members */
+	private int asInt;         /*  */
+	private String asDayTime;  /* will be on character from {M,T,W,H,F,S,U} followed by four numeric digits representing the time, based on a 24-hour clock */
+	
+	/* constructors */
+	public Time(int i){
+		asInt = i;
+		setDayTime();
+	}
+	
+	public Time(String s){
+		asDayTime = s;
+		setInt();
+	}
+	
+	/* getters */
+	public int getInt(){ return asInt; }
+	public String getDayTime(){ return asDayTime; }
+	
+	/* setters */
+	public void setInt(){
+		char day = asDayTime.charAt(0);
+		int base;
+		
+		switch(day){
+		case 'M': base = 0;
+		break;
+		case 'T': base = 1;
+		break;
+		case 'W': base = 2;
+		break;
+		case 'H': base = 3;
+		break;
+		case 'F': base = 4;
+		break;
+		case 'S': base = 5;
+		break;
+		case 'U': base = 6;
+		break;
+		default: base = -1;  /* if this happens, something is wrong */
+		break;
+		}
+		
+		this.asInt = (base * 10000) + Integer.parseInt(this.asDayTime.substring(1));
+	}
+	
+	public void setDayTime(){
+		int dayAsInt = this.asInt / 10000;
+		char day;
+		
+		switch(dayAsInt){
+		case 0: day = 'M';
+		break;
+		case 1: day = 'T';
+		break;
+		case 2: day = 'W';
+		break;
+		case 3: day = 'H';
+		break;
+		case 4: day = 'F';
+		break;
+		case 5: day = 'S';
+		break;
+		case 6: day = 'U';
+		break;
+		default: day = 'X'; /* if this happens, something is wrong */
+		break;
+		}
+		
+		String timeAsString = Integer.toString(this.asInt % 10000);
+		while(timeAsString.length() < 4)
+			timeAsString = '0' + timeAsString;
+		
+		this.asDayTime = (day + timeAsString);
+	}
 }
