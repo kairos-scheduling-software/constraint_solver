fc15feffe7aeb2242fcf8047ed31ee46e511cbb2
Tony <tony.tuttle@utah.edu>
2015-02-03 16:53:34 -0700

    changes from yesterday

--- src/scheduleSolver/Space.java
+++ src/scheduleSolver/Space.java
@@ -5,20 +5,17 @@ import java.util.ArrayList;
 public class Space {
 	
 	/* members */
-	String name;                     /* human readable name, e.g. WEBL101 */
 	int ID;	                     	 /* used as part of the int representation for the solver */
 	int capacity;                    /* max number of attendees in the room (excludes administrative people such as teachers, speakers, etc) */
 	ArrayList<Time> timesAvailable;  /* the times at which the Space is available, by default it is from Monday at midnight to Sunday at 23:59 */
 	ArrayList<Event> events;		 /* the events that are actually scheduled in this Space */
 	
 	/* constructors */
-	public Space(String n, int c){
-		name = n;
+	public Space(int c){
 		capacity = c;
 	}
 	
 	/* getters */
-	public String getName(){ return name; }
 	public int getID(){ return ID; }
 	public int getCapacity(){ return capacity; }
 	
