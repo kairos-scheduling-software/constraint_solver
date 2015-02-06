b1dbef57549c4a617bd4a469b6280d9fb1326fca
Vinh Dang <dtt.vinh@gmail.com>
2015-02-06 12:28:34 -0700

    Add getters and restructure Event class.

--- src/scheduleSolver/Event.java
+++ src/scheduleSolver/Event.java
@@ -2,7 +2,6 @@ package scheduleSolver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,18 +35,27 @@ public class Event {
 	Solver solver;
 	IntVar startTime;
 	ArrayList<IntVar> blocks;
-	IntVar spaceId;
+	IntVar spaceId = null;
 	
-	public Event(int id, int capacity, int daysCount, int duration, 
+	public Event(int id, int maxParticipants, int daysCount, int duration, 
 			String[] possibleStartTimes, int personId) {
+		this(id, maxParticipants, daysCount, duration, 
+			possibleStartTimes, personId, -1);
+	}
+	
+	public Event(int id, int maxParticipants, int daysCount, int duration, 
+			String[] possibleStartTimes, int personId, int spaceId) {
 		this.ID = id;
 		this.daysCount = daysCount;
 		this.duration = duration;
-		this.maxParticipants = capacity;
+		this.maxParticipants = maxParticipants;
 		this.personId = personId;
 		this.possibleStartTimes = new ArrayList<Time>();
 		for (String t : possibleStartTimes) {
 			this.possibleStartTimes.add(new Time(t));
+		}
+		if (spaceId >= 0) {
+			this.spaceIds = new int[]{spaceId};
 		}
 	}
 	
@@ -69,11 +77,28 @@ public class Event {
 	
 	public void initialize(Solver solver, Map<Integer, Space> spaces) {
 		this.solver = solver;
-		int[] spaceIds = Ints.toArray(getSpaceIds(spaces));
-		this.spaceId = VariableFactory.enumerated("room id", spaceIds, this.solver);
 		
-		this.startTime = getStartTime();
-		this.blocks = getBlocks();
+		// Initialize Space Id
+		ArrayList<Integer> spaceIds = new ArrayList<Integer>();
+		if (this.spaceIds == null) {
+			for (Map.Entry<Integer, Space> entry : spaces.entrySet()) {
+				if (entry.getValue().capacity >= this.maxParticipants) {
+					spaceIds.add(entry.getKey());
+				}
+			}
+		} else {
+			for (int i : this.spaceIds) {
+				if (spaces.get(i).capacity >= this.maxParticipants) {
+					spaceIds.add(i);
+				}
+			}
+		}
+		this.spaceIds = Ints.toArray(spaceIds);
+		this.spaceId = VariableFactory.enumerated("room id", this.spaceIds, this.solver);
+		
+		// Initialize Start Time
+		initializeStartTime();
+		initializeBlocks();
 	}
 	
 	public Constraint notOverlap(Event other) {
@@ -85,6 +110,27 @@ public class Event {
 			return _time;
 		else
 			return LogicalConstraintFactory.or(_space, _time);
+	}
+	
+	public int getId() { return this.ID; }
+	public int getSpaceId() { return this.spaceId.getValue(); }
+	public char[] getDays() {
+		char[] dayArr = {'M', 'T', 'W', 'H', 'F', 'S', 'U'};
+		char[] days = new char[this.daysCount];
+		Time startTime = new Time(this.startTime.getValue());
+		String strStartTime = startTime.getDayTime();
+		int dayIndex = Arrays.binarySearch(dayArr, strStartTime.charAt(0));
+		
+		// May get IndexOutOfBound due to malfunction-input
+		for (int i = 0; i < days.length; i++) {
+			days[i] = dayArr[dayIndex + i * 2];
+		}
+		
+		return days;
+	}
+	public String getStartTime() {
+		Time startTime = new Time(this.startTime.getValue());
+		return startTime.getDayTime().substring(1);
 	}
 	
 	public static Map<Integer, Event> parseClasses(Solver _solver,
@@ -109,27 +155,25 @@ public class Event {
 		return result;
 	}
 	
-	private ArrayList<Integer> getSpaceIds(Map<Integer, Space> spaces) {
-		ArrayList<Integer> ids = new ArrayList<Integer>();
-		for (Map.Entry<Integer, Space> entry : spaces.entrySet()) {
-			if (entry.getValue().capacity >= this.maxParticipants) {
-				ids.add(entry.getKey());
-			}
-		}
-		return ids;
-	}
+//	private ArrayList<Integer> getSpaceIds(Map<Integer, Space> spaces) {
+//		ArrayList<Integer> ids = new ArrayList<Integer>();
+//		for (Map.Entry<Integer, Space> entry : spaces.entrySet()) {
+//			if (entry.getValue().capacity >= this.maxParticipants) {
+//				ids.add(entry.getKey());
+//			}
+//		}
+//		return ids;
+//	}
 	
-	private IntVar getStartTime() {
-		IntVar result = null;
+	private void initializeStartTime() {
 		int[] tmp = new int[this.possibleStartTimes.size()];
 		for (int i = 0; i < tmp.length; i++) {
 			tmp[i] = this.possibleStartTimes.get(i).getInt();
 		}
-		result = VariableFactory.enumerated("possible start times", tmp, this.solver);
-		return result;
+		this.startTime = VariableFactory.enumerated("possible start times", tmp, this.solver);
 	}
 	
-	private ArrayList<IntVar> getBlocks() {
+	private void initializeBlocks() {
 		int n = this.duration;
 		ArrayList<IntVar> _blocks = new ArrayList<IntVar>();
 		for (int i = 0; i < this.daysCount; i++) {
@@ -138,6 +182,6 @@ public class Event {
 			}
 		}
 		
-		return _blocks;
+		this.blocks = _blocks;
 	}
 }
