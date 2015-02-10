8dbef02df33ff1feec9fde470433edd8d7fe41a2
Vinh Dang <dtt.vinh@gmail.com>
2015-02-09 22:20:27 -0700

    Change server's port config to work with heroku.

--- src/main/java/schedulingServer/Main.java
+++ src/main/java/schedulingServer/Main.java
@@ -67,7 +67,8 @@ public class Main extends HttpServlet {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		//Server server = new Server(8080);
 		ServletContextHandler context = new ServletContextHandler(
 				ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
